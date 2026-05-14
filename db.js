1591be1ea26afa0fda4b91d30ffb77dfd4cb0e4f
Caspar Kroll <mail@casparkroll.de>
2026-05-14 15:12:42 +0200

    Updated db to use combined meals schema and provide location lookup

--- db.js
+++ db.js
@@ -27,70 +27,110 @@ const db = new DatabaseSync('database.db');
 
 // create schema
 db.exec(`
-  CREATE TABLE IF NOT EXISTS trans (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    mandantId INTEGER,
-    transFullId TEXT NOT NULL UNIQUE,
-    datum INTEGER NOT NULL,
-    ortName TEXT,
-    kaName TEXT,
-    typName TEXT,
-    zahlBetrag REAL,
-    dateiablageId INTEGER,
-    bonusInfo TEXT,
-    cardnumber TEXT NOT NULL,
-    UNIQUE(transFullId, datum, cardnumber)
-  );
-  CREATE INDEX IF NOT EXISTS idx_trans_cardnumber ON trans (cardnumber);
-  CREATE INDEX IF NOT EXISTS idx_trans_transFullId ON trans (transFullId);
-  CREATE INDEX IF NOT EXISTS idx_trans_datum ON trans (datum);
-  CREATE INDEX IF NOT EXISTS idx_trans_ortName ON trans (ortName);
+    CREATE TABLE IF NOT EXISTS trans (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        mandantId INTEGER,
+        transFullId TEXT NOT NULL UNIQUE,
+        datum INTEGER NOT NULL,
+        ortName TEXT,
+        kaName TEXT,
+        typName TEXT,
+        zahlBetrag REAL,
+        dateiablageId INTEGER,
+        bonusInfo TEXT,
+        cardnumber TEXT NOT NULL,
+        UNIQUE(transFullId, datum, cardnumber)
+    );
+    CREATE INDEX IF NOT EXISTS idx_trans_cardnumber ON trans (cardnumber);
+    CREATE INDEX IF NOT EXISTS idx_trans_transFullId ON trans (transFullId);
+    CREATE INDEX IF NOT EXISTS idx_trans_datum ON trans (datum);
+    CREATE INDEX IF NOT EXISTS idx_trans_ortName ON trans (ortName);
 `);
 db.exec(`
-  CREATE TABLE IF NOT EXISTS transpos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    mandantId INTEGER,
-    transFullId TEXT NOT NULL,
-    posId INTEGER,
-    name TEXT,
-    menge INTEGER,
-    epreis REAL,
-    rabatt REAL,
-    gpreis REAL,
-    bewertung INTEGER,
-    cardnumber TEXT NOT NULL,
-    UNIQUE(transFullId, posId, cardnumber)
-  );
+    CREATE TABLE IF NOT EXISTS transpos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        mandantId INTEGER,
+        transFullId TEXT NOT NULL,
+        posId INTEGER,
+        name TEXT,
+        menge INTEGER,
+        epreis REAL,
+        rabatt REAL,
+        gpreis REAL,
+        bewertung INTEGER,
+        cardnumber TEXT NOT NULL,
+        UNIQUE(transFullId, posId, cardnumber)
+    );
     CREATE INDEX IF NOT EXISTS idx_transpos_cardnumber ON transpos (cardnumber);
     CREATE INDEX IF NOT EXISTS idx_transpos_transFullId ON transpos (transFullId);
 `);
 db.exec(`
-  CREATE TABLE IF NOT EXISTS openmensa_meals (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    notes TEXT,
-    prices TEXT,
-    category TEXT NOT NULL,
-    date INTEGER NOT NULL,
-    canteenId INTEGER NOT NULL,
-    UNIQUE(name, date, canteenId)
-  );
+    CREATE TABLE IF NOT EXISTS mensa_locations (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        openMensaId INTEGER NOT NULL UNIQUE,
+        mensaXMLId INTEGER UNIQUE,
+        name TEXT NOT NULL,
+        internalName TEXT
+    );
+    INSERT OR IGNORE INTO mensa_locations (openMensaId, mensaXMLId, name, internalName) VALUES 
+    (63, 106, 'Mensa am Park', 'MaP Mensaküche Leitung'),
+    (64, 118, 'Mensa Academica', NULL),
+    (65, 115, 'Mensa am Elsterbecken', 'Mensaküche amElsterbecken'),
+    (66, 170, 'Mensa An den Tierkliniken', NULL),
+    (68, 111, 'Mensa Petersteinweg', 'PSW Mensaküche Leitung'),
+    (67, 162, 'Mensa Liebigstraße/Mensa und Cafeteria am Medizincampus', NULL),
+    (69, 140, 'Mensa/Cafetaria Schönauer Straße', NULL),
+    (70, 153, 'Cafeteria Dittrichring', NULL),
+    (71, null, 'Cafeteria Koburger Straße', NULL),
+    (72, 127, 'Cafeteria Philipp-Rosenthal-Straße/Mensaria am Botanischen Garten', NULL),
+    (73, null, 'Cafeteria Wächterstraße', NULL);
+`);
+db.exec(`
+    CREATE TABLE IF NOT EXISTS meals (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        mensa_location_id INTEGER NOT NULL,
+        date INTEGER NOT NULL,
+        name TEXT NOT NULL,
+        category TEXT NOT NULL,
+        internalCategory TEXT,
+        prices TEXT,
+        notes TEXT,
+        components TEXT,
+        tags TEXT,
+        FOREIGN KEY (mensa_location_id) REFERENCES mensa_locations(id),
+        UNIQUE(mensa_location_id, date, name, category)
+    );
+    CREATE INDEX IF NOT EXISTS idx_meals_mensa_location_id ON meals (mensa_location_id);
+    CREATE INDEX IF NOT EXISTS idx_meals_date ON meals (date);
+    CREATE INDEX IF NOT EXISTS idx_meals_internalCategory ON meals (internalCategory);
+`);
+db.exec(`
+    CREATE TABLE IF NOT EXISTS openmensa_meals (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        notes TEXT,
+        prices TEXT,
+        category TEXT NOT NULL,
+        date INTEGER NOT NULL,
+        canteenId INTEGER NOT NULL,
+        UNIQUE(name, date, canteenId)
+    );
     CREATE INDEX IF NOT EXISTS idx_openmensa_meals_date ON openmensa_meals (date);
     CREATE INDEX IF NOT EXISTS idx_openmensa_meals_canteenId ON openmensa_meals (canteenId);
 `);
 db.exec(`
-  CREATE TABLE IF NOT EXISTS mensa_xml_cache (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    locationId INTEGER NOT NULL,
-    date INTEGER NOT NULL,
-    name TEXT NOT NULL,
-    category TEXT NOT NULL,
-    internalCategory TEXT,
-    prices TEXT,
-    components TEXT,
-    tags TEXT,
-    UNIQUE(locationId, date, name, category)
-  );
+    CREATE TABLE IF NOT EXISTS mensa_xml_cache (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        locationId INTEGER NOT NULL,
+        date INTEGER NOT NULL,
+        name TEXT NOT NULL,
+        category TEXT NOT NULL,
+        internalCategory TEXT,
+        prices TEXT,
+        components TEXT,
+        tags TEXT,
+        UNIQUE(locationId, date, name, category)
+    );
     CREATE INDEX IF NOT EXISTS idx_mensa_xml_cache_locationId ON mensa_xml_cache (locationId);
     CREATE INDEX IF NOT EXISTS idx_mensa_xml_cache_date ON mensa_xml_cache (date);
     CREATE INDEX IF NOT EXISTS idx_mensa_xml_cache_internalCategory ON mensa_xml_cache (internalCategory);
@@ -105,10 +145,10 @@ db.exec(`
 export async function insertCard(cardnumber, passwort) {
     try {
         await client.secrets().getSecret({
-        environment: process.env.INFISICAL_ENVIRONMENT || "dev",
-        projectId: process.env.INFISICAL_PROJECT_ID,
-        secretName: cardnumber,
-        viewSecretValue: false
+            environment: process.env.INFISICAL_ENVIRONMENT || "dev",
+            projectId: process.env.INFISICAL_PROJECT_ID,
+            secretName: cardnumber,
+            viewSecretValue: false
         });
     } catch (error) {
         await client.secrets().createSecret(cardnumber, {
@@ -215,7 +255,7 @@ export function getTransList(cardnumber) {
         var results = stmt.all();
     } else {
         var stmt = db.prepare('SELECT * FROM trans WHERE cardnumber = ?');
-    var results = stmt.all(cardnumber);
+        var results = stmt.all(cardnumber);
     }
     return results.map(r => ({
         id: r.id,
@@ -290,34 +330,99 @@ export function getTransPosList(cardnumber) {
     }));
 }
 
+export function getMensaLocations() {
+    const stmt = db.prepare('SELECT * FROM mensa_locations');
+    const results = stmt.all();
+    return results.map(r => ({
+        id: r.id,
+        openMensaId: r.openMensaId,
+        mensaXMLId: r.mensaXMLId,
+        name: r.name,
+        internalName: r.internalName
+    }));
+}
+
+export function getMensaLocationByOpenMensaId(openMensaId) {
+    const stmt = db.prepare('SELECT * FROM mensa_locations WHERE openMensaId = ?');
+    const result = stmt.get(openMensaId);
+    if (result) {
+        return {
+            id: result.id,
+            openMensaId: result.openMensaId,
+            mensaXMLId: result.mensaXMLId,
+            name: result.name,
+            internalName: result.internalName
+        };
+    } else {
+        return null;
+    }
+}
+
+export function getMensaLocationByMensaXMLId(mensaXMLId) {
+    const stmt = db.prepare('SELECT * FROM mensa_locations WHERE mensaXMLId = ?');
+    const result = stmt.get(mensaXMLId);
+    if (result) {
+        return {
+            id: result.id,
+            openMensaId: result.openMensaId,
+            mensaXMLId: result.mensaXMLId,
+            name: result.name,
+            internalName: result.internalName
+        };
+    } else {
+        return null;
+    }
+}
+
+/**
+ * @typedef {Object} OpenMensaMeal
+ * @property {number} id
+ * @property {string} name
+ * @property {string} category
+ * @property {Object} prices
+ * @property {number|null} prices.students
+ * @property {number|null} prices.employees
+ * @property {number|null} prices.others
+ * @property {number|null} prices.pupils
+ * @property {string[]} notes
+ */
+/**
+ * 
+ * @param {OpenMensaMeal[]} meals 
+ * @param {number|string} canteenId 
+ * @param {string} date 
+ */
 export function insertOpenMensaMeals(meals, canteenId, date) {
     // parse date yyyy-mm-dd to timestamp
     var date = new Date(date);
+    var mensaLocationId = getMensaLocationByOpenMensaId(canteenId).id;
+    
     const stmt = db.prepare(`
-        INSERT OR IGNORE INTO openmensa_meals (name, notes, prices, category, date, canteenId)
+        INSERT OR IGNORE INTO meals (mensa_location_id, date, name, category, prices, notes)
         VALUES (?, ?, ?, ?, ?, ?)
     `);
     for (const meal of meals) {
         stmt.run(
-            meal.name,
-            meal.notes ? JSON.stringify(meal.notes) : null,
-            meal.prices ? JSON.stringify(meal.prices) : null,
-            meal.category,
+            mensaLocationId,
             date.getTime(),
-            canteenId
+            meal.name,
+            meal.category,
+            meal.prices ? JSON.stringify(meal.prices) : null,
+            meal.notes ? JSON.stringify(meal.notes) : null
         );
     }
 }
 
 export function getOpenMensaDays(canteenId, startDate = null) {
     var startDate = startDate ? new Date(startDate) : new Date();
-    const stmt = db.prepare('SELECT DISTINCT date FROM openmensa_meals WHERE canteenId = ? AND date >= ?');
-    var results = stmt.all(canteenId, startDate.getTime());
+    var mensaLocationId = getMensaLocationByOpenMensaId(canteenId).id;
+    const stmt = db.prepare('SELECT DISTINCT date FROM meals WHERE mensa_location_id = ? AND date >= ?');
+    var results = stmt.all(mensaLocationId, startDate.getTime());
     return results.map(r => new Date(r.date));
 }
 
 export function getAllOpenMensaMeals() {
-    const stmt = db.prepare('SELECT * FROM openmensa_meals');
+    const stmt = db.prepare('SELECT * FROM meals');
     const results = stmt.all();
     return results.map(r => ({
         id: r.id,
@@ -326,14 +431,15 @@ export function getAllOpenMensaMeals() {
         prices: r.prices ? JSON.parse(r.prices) : null,
         category: r.category,
         date: new Date(r.date),
-        canteenId: r.canteenId
+        canteenId: r.mensa_location_id
     }));
 }
 
 export function getOpenMensaMeals(canteenId, date) {
     var date = new Date(date);
-    const stmt = db.prepare('SELECT * FROM openmensa_meals WHERE canteenId = ? AND date = ?');
-    var results = stmt.all(canteenId, date.getTime());
+    var mensaLocationId = getMensaLocationByOpenMensaId(canteenId).id;
+    const stmt = db.prepare('SELECT * FROM meals WHERE mensa_location_id = ? AND date = ?');
+    var results = stmt.all(mensaLocationId, date.getTime());
     if (results.length > 0) {
         return results.map(r => ({
             id: r.id,
@@ -342,7 +448,7 @@ export function getOpenMensaMeals(canteenId, date) {
             prices: r.prices ? JSON.parse(r.prices) : null,
             category: r.category,
             date: new Date(r.date),
-            canteenId: r.canteenId
+            canteenId: r.mensa_location_id
         }));
     } else {
         return [];
@@ -351,12 +457,13 @@ export function getOpenMensaMeals(canteenId, date) {
 
 export function insertMensaXMLMeals(meals) {
     const stmt = db.prepare(`
-        INSERT OR IGNORE INTO mensa_xml_cache (locationId, date, name, category, internalCategory, prices, components, tags)
+        INSERT OR IGNORE INTO meals (mensa_location_id, date, name, category, internalCategory, prices, components, tags)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?)
     `);
     for (const meal of meals) {
+        var mensaLocationId = getMensaLocationByMensaXMLId(meal.locationId).id;
         stmt.run(
-            meal.locationId,
+            mensaLocationId,
             meal.date.getTime(),
             meal.name,
             meal.category,
@@ -369,11 +476,11 @@ export function insertMensaXMLMeals(meals) {
 }
 
 export function getAllMensaXMLMeals() {
-    const stmt = db.prepare('SELECT * FROM mensa_xml_cache');
+    const stmt = db.prepare('SELECT * FROM meals');
     const results = stmt.all();
     return results.map(r => ({
         id: r.id,
-        locationId: r.locationId,
+        locationId: r.mensa_location_id,
         date: new Date(r.date),
         name: r.name,
         category: r.category,
@@ -386,12 +493,13 @@ export function getAllMensaXMLMeals() {
 
 export function getMensaXMLMeals(canteenId, date) {
     var date = new Date(date);
-    const stmt = db.prepare('SELECT * FROM mensa_xml_cache WHERE locationId = ? AND date = ?');
-    var results = stmt.all(canteenId, date.getTime());
+    var mensaLocationId = getMensaLocationByMensaXMLId(canteenId).id;
+    const stmt = db.prepare('SELECT * FROM meals WHERE mensa_location_id = ? AND date = ?');
+    var results = stmt.all(mensaLocationId, date.getTime());
     if (results.length > 0) {
         return results.map(r => ({
             id: r.id,
-            locationId: r.locationId,
+            locationId: r.mensa_location_id,
             date: new Date(r.date),
             name: r.name,
             category: r.category,
