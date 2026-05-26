ade3780a8445d08a32a2443e4cb46d1435d972e4
Caspar Kroll <ckroll@schulzentrum.de>
2026-05-26 23:46:55 +0200

    added auto redirect to static routes

--- server.js
+++ server.js
@@ -35,14 +35,16 @@ await fastify.register(fastifySSE);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 await fastify.register(fastifyStatic, {
     root: path.join(__dirname, 'docs'),
-    prefix: '/docs/',
+    prefix: '/docs',
     index: 'index.html',
+    redirect: true
 });
 await fastify.register(fastifyStatic, {
     root: path.join(__dirname, 'ui'),
-    prefix: '/ui/',
+    prefix: '/ui',
     index: 'index.html',
     decorateReply: false,
+    redirect: true
 });
 
 /**
